8a92a19ebce55028f71e30da4558602353416f91
Didox <dansantos@abril.com.br>
2016-02-19 15:29:31 -0200

    desativar create

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ var HomeController = require('../controllers/home_controller');
 
 router.get('/', HomeController.index);
 // router.get('/create', HomeController.create);
-router.get('/insert', HomeController.insert);
+// router.get('/insert', HomeController.insert);
 router.get('/select', HomeController.select);
 
 module.exports = router;
